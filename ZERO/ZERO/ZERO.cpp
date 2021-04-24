19d1759dbe3614a94c478e8c283206a207efd82d
OhJunhyeok9912 <joonlove1234@gmail.com>
2021-04-24 17:36:39 +0900

    Zero 문제

---
+++ ZERO/ZERO/ZERO.cpp
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100000
+
+typedef int element;
+typedef struct {
+	element data[MAX];
+	int top;
+}Stack;
+
+void init_stack(Stack* s) {
+	s->top = -1;
+}
+
+int is_empty(Stack* s) {
+	return s->top == -1;
+}
+
+int is_full(Stack* s) {
+	return s->top == MAX - 1;
+}
+
+void push(Stack* s, element item) {
+	if (is_full(s)) return;
+	else s->data[++(s->top)] = item;
+}
+
+element pop(Stack* s) {
+	if (is_empty(s)) exit(1);
+	else return s->data[(s->top)--];
+}
+
+int main() {
+	int k, num;
+	int sum = 0;
+	Stack s; init_stack(&s);
+	scanf_s("%d", &k);
+	for (int i = 0; i < k; i++) {
+		scanf_s("%d", &num);
+		if (num == 0) pop(&s);
+		else push(&s, num);
+	}
+	for (int i = s.top; i >= 0; i--) {
+		sum += pop(&s);
+	}
+	printf("\n%d\n", sum);
+	return 0;
+}
